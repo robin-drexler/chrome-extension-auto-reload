475f4c1d8b6431d3f7b408c4f3cf1e1408f95bbf
Robin Drexler <drexler.robin@gmail.com>
2015-02-25 21:38:20 +0100

    the skateboard
    extension receives pings from socket.io and reloads chrome://extension tab
    or creates and reloads, if no tab was found

---
+++ src/js/background.js
@@ -0,0 +1,51 @@
+(function (console) {
+  "use strict";
+
+  const CHROME_EXTENSION_URL = 'chrome://extensions/';
+  const SOCKET_IO_PORT = '8890';
+
+  var io = require('./external/socket.io.js');
+  var socket = io('http://localhost:' + SOCKET_IO_PORT);
+
+
+  function reloadTab(tab) {
+    console.log('reloading tab', tab);
+    chrome.tabs.reload(tab.id);
+  }
+
+  function reloadExtensions() {
+    // search for any open extension tab and reload
+    chrome.tabs.query({
+      url: CHROME_EXTENSION_URL
+    }, function (tabs) {
+      console.log('found tabs', tabs.length, tabs);
+
+      if (tabs.length) {
+        reloadTab(tabs[0]);
+      } else {
+        // no extension tab found. Create and reload
+        console.log('creating new tab');
+        chrome.tabs.create({
+              url: CHROME_EXTENSION_URL,
+              index: 0,
+              pinned: true,
+              active: false
+            }, function (tab) {
+              window.setTimeout(function () {
+                reloadTab(tab);
+              }, 500); //not sure why immediate reload does not seem to work...
+            }
+        );
+      }
+
+    });
+  }
+
+  socket.on('file.change', function () {
+    console.log('received ping');
+    reloadExtensions();
+  });
+
+})(window.console);
+
+
