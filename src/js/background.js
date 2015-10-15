7a293bb7d0f6d3fd63a3c3ff37742374702549f4
Jerome Dane <jerome.dane@gmail.com>
2015-10-15 15:46:31 -0400

    Updated reload extensions method using code from https://github.com/arikw/chrome-extensions-reloader

--- src/js/background.js
+++ src/js/background.js
@@ -7,40 +7,39 @@
   var io = require('./external/socket.io.js');
   var socket = io('http://localhost:' + SOCKET_IO_PORT);
 
-
   function reloadTab(tab) {
     console.log('reloading tab', tab);
     chrome.tabs.reload(tab.id);
   }
 
-  function reloadExtensions() {
-    // search for any open extension tab and reload
-    chrome.tabs.query({
-      url: CHROME_EXTENSION_URL
-    }, function (tabs) {
-      console.log('found tabs', tabs.length, tabs);
+	function reloadExtensions() {
+		// find all unpacked extensions and reload them
+		chrome.management.getAll(function(a) {
+			var ext = {};
+			for (var i = 0; i < a.length; i++) {
+				ext = a[i];
+				if ((ext.name !== 'Chrome Unpacked Extension Auto Reload') &&	// don't reload yourself
+					(ext.installType=="development") &&
+					(ext.enabled == true) &&
+					(ext.name != "Extensions Reloader")) {
+						console.log(ext.name + " reloaded");
+						(function (extensionId, extensionType) {
+							// disable
+							chrome.management.setEnabled(extensionId, false, function() {
+								// re-enable
+								chrome.management.setEnabled(extensionId, true, function() {
+									// re-launch packaged app
+									if (extensionType == "packaged_app") {
+										chrome.management.launchApp(extensionId);
+									}
+								});
+							});
+						})(ext.id, ext.type);
+				}
+			}
+		});
 
-      if (tabs.length) {
-        reloadTab(tabs[0]);
-      } else {
-        // no extension tab found. Create and reload
-        console.log('creating new tab');
-        chrome.tabs.create({
-              url: CHROME_EXTENSION_URL,
-              index: 0,
-              pinned: true,
-              active: false
-            }, function (tab) {
-              window.setTimeout(function () {
-                reloadTab(tab);
-              }, 500); //not sure why immediate reload does not seem to work...
-              // note to self, probably one has to wait for load event, so it can be actually *re*loaded
-            }
-        );
-      }
-
-    });
-  }
+	}
 
   socket.on('file.change', function () {
     console.log('received ping');
